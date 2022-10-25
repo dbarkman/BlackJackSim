1e07298609c66ab9cff9c8c38c7ff356794d429d
David Barkman <david.barkman13@gmail.com>
2022-10-24 22:40:21 -0700

    Cleaned up lots of bugs, logic seems sound.

--- bjSimNew.php
+++ bjSimNew.php
@@ -27,6 +27,7 @@ class BlackJack
     private $dealerCards;
     private $dealerCard1;
     private $dealerTotal;
+    private $dealerBlackjack;
     private $playerMove;
     private $aces;
     private $splitCounter;
@@ -42,6 +43,7 @@ class BlackJack
         $this->playerCards = array();
         $this->handTotals = array();
         $this->dealerCards = array();
+        $this->dealerBlackjack = false;
 
         $this->verbose = true;
     }
@@ -58,7 +60,7 @@ class BlackJack
             $this->shoe = array_merge($this->shoe, $this->deck);
         }
         shuffle($this->shoe);
-        array_unshift($this->shoe, 2,8,2,10);
+        array_unshift($this->shoe, 7,11,10);
     }
     
     private function dealCards() {
@@ -81,33 +83,42 @@ class BlackJack
 
         if ($dealerCard2 == 11 && $this->dealerCard1 == 11) $dealerCard2 = 1;
         $this->dealerTotal = array_sum($this->dealerCards);
+        if ($this->dealerTotal == 21) $this->dealerBlackjack = true;
     }
     
     private function playHands() {
         foreach ($this->playerHands as &$hand) {
-            echo "----------" . PHP_EOL;
-            //clean up hand need a sum or ordered cards
-            if (count($hand) < 2) {
-                $newCard = array_shift($this->shoe);
-                $this->out("Player draws: " . $newCard . ", for " . $this->playerTotal);
-                $hand[] = $newCard;
+            if ($this->dealerBlackjack) {
+                $handTotal = array_sum($hand);
+                if ($handTotal == 22) $handTotal = 12;
+                $this->handTotals[] = $handTotal;
+            } else {
+                $this->out("----------");
+                if (count($hand) < 2) {
+                    $newCard = array_shift($this->shoe);
+                    $hand[] = $newCard;
+                    $this->out("Player draws: " . $newCard . ", for " . array_sum($hand));
+                }
+                $this->playerCards = $hand;
+                $this->cleanupHand();
+
+                $playerBlackjack = false;
+                if ($this->playerTotal == 1110) {
+                    $this->out("BLACKJACK!");
+                    $playerBlackjack = true;
+                }
+
+                if (!$playerBlackjack) {
+                    $this->determineStrategy();
+                    $this->playHand();
+                } else {
+                    $this->handTotals[] = '1';
+                }
             }
-            $this->playerCards = $hand;
-            $this->cleanupHand();
-
-            if ($this->playerTotal == 21) {
-                echo "BLACKJACK!" . PHP_EOL;
-            }
-
-            //player decides strategy, needs playerTotal
-            $this->determineStrategy();
-
-            //player plays
-            $this->playHand();
         } //end of loop
 
         //dealer plays
-        echo "----------" . PHP_EOL;
+        $this->out("----------");
         if ($this->dealerTotal == 22) {
             unset($this->dealerCards[1]);
             $this->dealerCards[1] = 1;
@@ -116,111 +127,96 @@ class BlackJack
         if ($this->dealerTotal <= 17) {
             $this->dealerDraws();
         }
-        echo "Dealer stands with: " . $this->dealerTotal . PHP_EOL;
+        $this->out("Dealer stands with: " . $this->dealerTotal);
     }
 
     private function playHand() {
-        //stand
         if ($this->playerMove == "S") {
             if ($this->playerTotal > 0) {
-                echo "Player will stand with: " . $this->playerTotal . PHP_EOL;
+                $this->out("Player will stand with: " . $this->playerTotal);
             }
             $this->handTotals[] = $this->playerTotal;
 
         } else if ($this->playerMove == "H") {
-            echo "Player will hit with: " . $this->playerTotal . PHP_EOL;
+            $this->out("Player will hit with: " . $this->playerTotal);
             while ($this->playerTotal <= 21) {
                 $newCard = array_shift($this->shoe);
                 $originalNewCard = $newCard;
                 if ($newCard == 11 && $this->playerTotal > 10) $newCard = 1;
                 $this->playerCards[] = $newCard;
                 $this->playerTotal = array_sum($this->playerCards);
-                $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
                 if ($this->playerTotal > 21) {
                     $position = array_search(11, $this->playerCards);
                     if ($position !== FALSE) {
                         unset($this->playerCards[$position]);
                         $this->playerCards[$position] = 1;
                         $this->playerTotal = array_sum($this->playerCards);
+                        $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
                         $this->determineStrategy();
                         if ($this->playerMove == "S") break;
                     } else {
+                        $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
                         $this->playerMove = "S";
-//                        $this->playHand();
                     }
                 } else {
+                    $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
                     $this->determineStrategy();
                     if ($this->playerMove == "S") break;
-//                    $this->playHand();
                 }
             }
             $this->playHand();
 
-//////////////////////////
-//            echo "Player will hit with: " . $this->playerTotal . PHP_EOL;
-//            while ($this->playerTotal <= 21) {
-//                $newCard = array_shift($this->shoe);
-//                $this->outputToTerminal("Player draws: " . $newCard);
-//                if ($newCard == 11 && $this->playerTotal > 10) $newCard = 1;
-//                $this->playerCards[] = $newCard;
-//                $this->playerTotal = array_sum($this->playerCards);
-//                if ($this->playerTotal > 21) {
-//                    $this->playerMove = "S";
-////                    $this->playHand();
-//                } else {
-//                    $this->determineStrategy();
-//                    if ($this->playerMove == "S") break;
-////                    $this->playHand();
-//                }
-//            }
-//            $this->playHand();
-//////////////////////////
-
         } else if ($this->playerMove == "D") {
-            echo "Player will double down with: " . $this->playerTotal . PHP_EOL;
+            $this->out("Player will double down with: " . $this->playerTotal);
             $newCard = array_shift($this->shoe);
             $originalNewCard = $newCard;
             if ($newCard == 11 && $this->playerTotal > 10) $newCard = 1;
             $this->playerCards[] = $newCard;
             $this->playerTotal = array_sum($this->playerCards);
+            if ($this->playerTotal > 21) {
+                $position = array_search(11, $this->playerCards);
+                if ($position !== FALSE) {
+                    unset($this->playerCards[$position]);
+                    $this->playerCards[$position] = 1;
+                    $this->playerTotal = array_sum($this->playerCards);
+                }
+            }
             $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
             $this->playerMove = "S";
             $this->playHand();
 
         } else if ($this->playerMove == "P") {
-            echo "Player will split with: " . $this->playerTotal . PHP_EOL;
             if ($this->playerTotal == 22) { //just for aces
-                echo "Splitting Aces" . PHP_EOL;
+                $this->out("Splitting Aces");
                 foreach ($this->playerCards as $card) {
                     $newCard = array_shift($this->shoe);
                     $originalNewCard = $newCard;
                     if ($newCard == 11) $newCard = 1;
                     $this->playerCards = [$card, $newCard];
                     $this->playerTotal = array_sum($this->playerCards);
+                    $this->out("----------");
                     $this->out("Player draws: " . $originalNewCard . ", for " . $this->playerTotal);
                     $this->playerMove = "S";
                     $this->playHand();
                 }
             } else {
-                $this->splitCounter++;
                 if ($this->splitCounter < 3) {
                     $secondCard = $this->playerCards[1];
-                    echo "Splitting " . $secondCard . "s" . PHP_EOL;
+                    $this->out("Player is splitting " . $secondCard . "s");
                     unset($this->playerCards[1]);
                     $newCard = array_shift($this->shoe);
                     $this->playerCards[1] = $newCard;
                     $this->out("Player draws: " . $newCard . ", for " . ($secondCard + $newCard));
                     $this->playerHands[] = [$secondCard];
                 }
-                //clean up hand
+                $this->splitCounter++;
                 $this->cleanupHand();
-                //determine strategy
                 $this->determineStrategy();
                 $this->playHand();
             }
 
         } else if ($this->playerMove == "Sr" && count($this->playerCards) == 2) {
-            echo "Player will surrender with: " . $this->playerTotal . ", against: " . $this->dealerCard1 . PHP_EOL;
+            $this->out("Player will surrender with: " . $this->playerTotal . ", against: " . $this->dealerCard1);
             $this->playerTotal = 0;
             $this->playerMove = "S";
             $this->playHand();
@@ -258,6 +254,7 @@ class BlackJack
         $this->importStrategy();
         $column = $this->dealerCard1 - 1;
         while ($row = fgetcsv($this->strategyFile)) {
+//            echo $row[0] . PHP_EOL;
             if ($row[0] == $this->playerTotal) {
                 $this->playerMove = $row[$column];
                 break;
@@ -274,26 +271,39 @@ class BlackJack
             if ($newCard == 11 && $this->dealerTotal > 10) $newCard = 1;
             $this->dealerCards[] = $newCard;
             $this->dealerTotal = array_sum($this->dealerCards);
+            if ($this->dealerTotal > 21) {
+                $position = array_search(11, $this->dealerCards);
+                if ($position !== FALSE) {
+                    unset($this->dealerCards[$position]);
+                    $this->dealerCards[$position] = 1;
+                    $this->dealerTotal = array_sum($this->dealerCards);
+                }
+            }
             $this->out($dealerDraw . ", for: " . $this->dealerTotal);
             if (in_array(11, $this->dealerCards) && $this->dealerTotal > 17) break;
         }
     }
 
     private function evaluateHands() {
-        echo "----------" . PHP_EOL;
+        $this->out("----------");
         foreach ($this->handTotals as $handTotal) {
+            if ($handTotal == 1 && $this->dealerTotal == 21) $handTotal = 21;
             if ($handTotal > 21) {
-                echo "Player looses, player busts. Player: " . $handTotal . ", Dealer: " . $this->dealerTotal . PHP_EOL;
+                $this->out("Player looses, player busts. Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
             } else if ($handTotal == 0) {
-                echo "Player surrenders." . PHP_EOL;
-            } else if ($this->dealerTotal > 21) {
-                echo "Player wins, dealer busts! Player: " . $handTotal . ", Dealer: " . $this->dealerTotal . PHP_EOL;
-            } else if ($this->dealerTotal > $handTotal) {
-                echo "Player looses. Player: " . $handTotal . ", Dealer: " . $this->dealerTotal . PHP_EOL;
+                $this->out("Player surrenders.");
+            } else if ($this->dealerTotal > 21 && $handTotal != 1) {
+                $this->out("Player wins, dealer busts! Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
+            } else if ($this->dealerBlackjack && $handTotal != 21) {
+                $this->out("Player looses, dealer hit blackjack. Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
+            } else if ($handTotal == 1 && $this->dealerTotal != 21) {
+                $this->out("Player wins, player hits Blackjack! Player: 21, Dealer: " . $this->dealerTotal);
             } else if ($handTotal > $this->dealerTotal) {
-                echo "Player wins! Player: " . $handTotal . ", Dealer: " . $this->dealerTotal . PHP_EOL;
+                $this->out("Player wins! Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
+            } else if ($this->dealerTotal > $handTotal) {
+                $this->out("Player looses. Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
             } else {
-                echo "Push Player: " . $handTotal . ", Dealer: " . $this->dealerTotal . PHP_EOL;
+                $this->out("Push Player: " . $handTotal . ", Dealer: " . $this->dealerTotal);
             }
         }
     }
